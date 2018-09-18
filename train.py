901a6feb4a390e7dabbce7c847cee0d5af00b182
wangyida <yidawang.cn@gmail.com>
2018-09-18 16:15:19 +0200

    fix conflict

--- train.py
+++ train.py
@@ -96,8 +96,12 @@ def train(n_epochs, learning_rate_G, learning_rate_D, batch_size, mid_flag,
 
     Z_tf_sample, vox_tf_sample = fcr_agan_model.samples_generator(
         visual_size=batch_size)
-    sample_vox_tf, sample_refine_vox_tf = fcr_agan_model.refine_generator(
-        visual_size=batch_size, tsdf=tsdf_tf)
+    if self.refiner is 'sscnet':
+        sample_vox_tf, sample_refine_vox_tf = fcr_agan_model.refine_generator_sscnet(
+            visual_size=batch_size, tsdf=tsdf_tf)
+    else:
+        sample_vox_tf, sample_refine_vox_tf = fcr_agan_model.refine_generator_resnet(
+            visual_size=batch_size, tsdf=tsdf_tf)
     writer = tf.summary.FileWriter(cfg.DIR.LOG_PATH, sess.graph_def)
     tf.initialize_all_variables().run(session=sess)
 
