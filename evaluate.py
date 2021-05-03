b454320061f5bf64f705e0dd8ad3cda33dca9f31
wangyida <yidawang.cn@gmail.com>
2021-05-03 16:05:39 +0200

    add some fusion point cloud

--- evaluate.py
+++ evaluate.py
@@ -243,7 +243,7 @@ def evaluate(batch_size, checknum, mode, discriminative, data_list):
         # decoded
         do_save_pcd = True
         if do_save_pcd is True:
-            results_pcds = np.argmax(pd_comp, axis=4)
+            results_pcds = np.argmax(pd_full, axis=4)
             # results_pcds = voxel_test
             for i in range(np.shape(results_pcds)[0]):
                 pcd_idx = np.where(results_pcds[i] > 0)
